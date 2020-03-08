bb38fcdd619efe955c22da6f315b68b4b9d42071
Shouvik Mukherjee <shouvik.shouvik@gmail.com>
2020-03-08 22:55:49 +0530

    Book list styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,11 +3,11 @@ import React from 'react'
 const BookList = (props) => {
   return (
     <div className="books">
-      {props.books.map((book, index) => <div key={index} style={{width: '33%', float: 'left'}}>
+      {props.books.map((book, index) => <div key={index} style={{width: '30%', float: 'left', padding: 10, margin: 10, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
         <h3>{book.title}</h3>
-        <p>{book.summary.summary.substring(1,30) + '...'}</p>
+        <p>{book.summary.summary.substring(1,230) + '...'}</p>
         <hr />
-        <p>{book.author.author}</p><hr /></div>)}
+        <p>{book.author.author}</p></div>)}
     </div>
   )
 }
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './styles/suggestions.css'
 
 const Suggestions = (props) => {
+  const [hover, setHover] = useState(false);
+  const toggleHover = () => {
+    setHover(!hover)
+    console.log(hover)
+  }
   return (
     <div style={{ width: 400, margin: 'auto' }}>
-      <div style={{ position: "absolute", width: 310,  marginLeft: 14, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
-        <ul style={{ listStyleType: 'none', textAlign: 'left', paddingLeft: 15, marginTop: 5 }}>
-          {props.books.map((book, index) => <li key={index} onClick={() => props.addBook(index)} style={{ backgroundColor: '#fff', width: '100%', padding: 5 }}>{book.title}</li>)}
+      <div style={{ position: "absolute", width: 310, marginLeft: 14, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
+        <ul style={{ listStyleType: 'none', textAlign: 'left', marginTop: 5, padding: 0 }}>
+          {props.books.map((book, index) =>
+            <li
+              key={index}
+              onClick={() => props.addBook(index)}
+              style={{ width: '95%'}}
+              className="listItem"
+            >{book.title}</li>)}
         </ul>
       </div>
     </div>
